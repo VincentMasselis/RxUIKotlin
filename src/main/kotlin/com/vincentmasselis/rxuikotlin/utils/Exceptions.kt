61192a789a6e49465bc4af860d5339cc9472817b
Vincent Masselis <vincent.masselis@gmail.com>
2017-09-21 19:00:10 +0200

    Inlined some exceptions

--- src/main/kotlin/com/vincentmasselis/rxuikotlin/utils/Exceptions.kt
+++ src/main/kotlin/com/vincentmasselis/rxuikotlin/utils/Exceptions.kt
@@ -3,14 +3,10 @@ package com.vincentmasselis.rxuikotlin.utils
 sealed class NoOpposite {
 
     class ActivityStateException(val state: ActivityState) : Throwable() {
-        override fun toString(): String {
-            return "NoOpposite.ActivityStateException(state=$state)"
-        }
+        override fun toString(): String = "NoOpposite.ActivityStateException(state=$state)"
     }
 
     class FragmentStateException(val state: FragmentState) : Throwable() {
-        override fun toString(): String {
-            return "NoOpposite.FragmentStateException(state=$state)"
-        }
+        override fun toString(): String = "NoOpposite.FragmentStateException(state=$state)"
     }
 }
