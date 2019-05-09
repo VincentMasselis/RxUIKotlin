7495e47dea3460382495d3f36f6b216280aeb6c9
Vincent Masselis <vincent@equisense.fr>
2019-05-09 17:54:38 +0200

    LifecycleViewHolder retrocompatibility

--- rxuikotlin-core/src/main/kotlin/com/vincentmasselis/rxuikotlin/LifecycleViewHolder.kt
+++ rxuikotlin-core/src/main/kotlin/com/vincentmasselis/rxuikotlin/LifecycleViewHolder.kt
@@ -3,6 +3,7 @@ package com.vincentmasselis.rxuikotlin
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 import java.util.concurrent.atomic.AtomicBoolean
 
@@ -16,9 +17,11 @@ abstract class LifecycleViewHolder(itemView: View) : RecyclerView.ViewHolder(ite
 
     open fun onWindowAttach() {
         onWindowAttachSubject.onNext(true)
+        onAttach()
     }
 
     open fun onWindowDetach() {
+        onDetach()
         onWindowAttachSubject.onNext(false)
     }
 
@@ -32,5 +35,36 @@ abstract class LifecycleViewHolder(itemView: View) : RecyclerView.ViewHolder(ite
 
     open fun onAdapterDetach() {
         onAdapterAttachSubject.onNext(false)
+    }
+
+    // -------------- Deprecated part
+    private var disps: CompositeDisposable? = null
+
+    @Synchronized
+    @Deprecated("onAttach is deprecated, now you have to use onWindowAttach() with disposeOnState() at the end", ReplaceWith("onWindowAttach()"))
+    open fun onAttach() {
+        disps = CompositeDisposable()
+        onAttach(disps!!)
+    }
+
+    @Synchronized
+    @Deprecated("onDetach is deprecated, now you have to use onWindowDetach() with disposeOnState() at the end", ReplaceWith("onWindowDetach()"))
+    open fun onDetach() {
+        disps!!.dispose()
+        disps = null
+    }
+
+    /**
+     * @return a [CompositeDisposable] if the current [LifecycleViewHolder] is between [onAttach] and [onDetach], returns null otherwise.
+     */
+    @Deprecated("disposable is deprecated, stop using it")
+    fun disposable(): CompositeDisposable? = disps
+
+    /**
+     * Called when the current [LifecycleViewHolder] view is attached to the window. Override this method and add every of your [io.reactivex.disposables.Disposable] to the filled
+     * [detachDisposable]. [detachDisposable.dispose()] is automatically called when the view is detached from the window.
+     */
+    @Deprecated("onAttach is deprecated", ReplaceWith("onWindowAttach()"))
+    open fun onAttach(detachDisposable: CompositeDisposable) {
     }
 }
